820d7793b9650085716a9ffd8ea0a345b2e0ca19
Bob Rosbag <mail@bobrosbag.nl>
2016-01-22 02:43:38 +0100

    Updated to new plugin style, compatible with v3, dummy mode and clean up of code

--- parallel_port_trigger/parallel_port_trigger.md
+++ parallel_port_trigger/parallel_port_trigger.md
@@ -3,7 +3,7 @@ OpenSesame Plug-in: Parallel Port Trigger
 
 *An OpenSesame Plug-in for sending stimulus synchronization triggers through the parallel port to data acquisition systems.*  
 
-Copyright, 2013, Bob Rosbag  
+Copyright, 2016, Bob Rosbag  
 
 Contributions: Code is based on the work of Per Sederberg. Debugged and polished by Edwin Dalmaijer.
 
@@ -12,11 +12,11 @@ Contributions: Code is based on the work of Per Sederberg. Debugged and polished
 
 In EEG/ERP studies it is common to send triggers to mark the timestamp for significant events (e.g., the onset of a trial, presentation of a particular stimulus, etc.). Triggers are typically bytes that are sent via the parallel port to data acquisition systems.
 
-The plug-in has three options:
-- *Value* is a positive integer between 0-255 and specifies the trigger byte
+The plug-in has four options:
+- *Value* is a positive integer between 1-255 and specifies the trigger byte
 - *Duration* (ms) is a positive integer, 'keypress', or 'mouseclick', to wait respectively for a specified interval in milliseconds, until a key has been pressed, or until a mouse button has been clicked. It specifies the time the trigger is active.
 - *Port adress (Windows only)* is the hexadecimal value of the parallel port in Windows. This setting is ignored in Linux.
-
+- *Dummy mode* for testing experiments
 
 Linux and Windows are supported (possible also OSX, not tested). For Windows the `DLPortIO.dll` driver is used to access the parallel port. Install options are listed below.
 
@@ -27,13 +27,14 @@ Installation instructions: <http://osdoc.cogsci.nl/devices/triggers/>
 2. LICENSE
 ----------
 
-Parallel Port Trigger is distributed under the terms of the GNU General Public License 3.
+The Parallel Port Trigger Plug-in is distributed under the terms of the GNU General Public License 3.
 The full license should be included in the file COPYING, or can be obtained from
 
 - <http://www.gnu.org/licenses/gpl.txt>
 
 Parallel Port Trigger contains works of others. For the full license information, please
 refer to `debian/copyright`.
+
 
 3. Documentation
 ----------------
--- parallel_port_trigger/parallel_port_trigger.py
+++ parallel_port_trigger/parallel_port_trigger.py
@@ -1,11 +1,9 @@
 #-*- coding:utf-8 -*-
 
 """
-
-25-06-2013
-This based on io_port of Per Sederberg
+21-01-2016
 Author: Bob Rosbag
-Author: Edwin Dalmaijer
+Version: 1.0
 
 This file is part of OpenSesame.
 
@@ -23,205 +21,128 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from libopensesame.item import item
-from libqtopensesame.items.qtautoplugin import qtautoplugin
-#from openexp.canvas import canvas
-#from PyQt4 import QtGui, QtCore
-
-from openexp.keyboard import keyboard
-import warnings
+#import warnings
 import os
 import imp
 
+from libopensesame.py3compat import *
+from libopensesame import debug
+from libopensesame.item import item
+from libqtopensesame.items.qtautoplugin import qtautoplugin
+
+#from openexp.keyboard import keyboard
 
 if os.name == 'posix':
-	# import the local modified version of pyparallel
-	# that allows for non-exclusive connections to the parport
-	path_to_file = os.path.join(os.path.dirname(__file__), "parallelppdev.py")
-	parallel = imp.load_source('parallel', path_to_file)
-	try: 
-		import parallelppdev as parallel
-	except ImportError:
-		print("The local modified version of pyparallel could not be loaded. Check if the file is present and if the file permissions are correct.")
-elif os.name == 'nt' :
-	try:
-		from ctypes import windll
-	except ImportError:
-		print("The ctypes module can not be loaded. Check if ctypes is installed correctly.")
+    # import the local modified version of pyparallel
+    # that allows for non-exclusive connections to the parport
+    path_to_file = os.path.join(os.path.dirname(__file__), 'parallelppdev.py')
+    parallel = imp.load_source('parallel', path_to_file)
+    try:
+        import parallelppdev as parallel
+    except ImportError:
+        print(u'The local modified version of pyparallel could not be loaded. Check if the file is present and if the file permissions are correct.')
+elif os.name == 'nt':
+    try:
+        from ctypes import windll
+    except ImportError:
+        print(u'The ctypes module can not be loaded. Check if ctypes is installed correctly.')
 else:
-	try:
-		import parallel
-	except ImportError:
-		print("The pyparallel module could not be loaded, please make sure pyparallel is installed correctly.")
-		
-
-# we only want one instance of pp, so here's a global var
-
-if os.name == 'nt':
-	_winpp = None
-else:
-	_pp = None
- 
- 
-
+    try:
+        import parallel
+    except ImportError:
+        print('The pyparallel module could not be loaded, please make sure pyparallel is installed correctly.')
 
 
 class parallel_port_trigger(item):
 
-	"""
-	This class (the class with the same name as the module) handles the basic
-	functionality of the item. It does not deal with GUI stuff.
-	"""
+    """
+    Parallel Port Trigger class handles the basic functionality of the item.
+    It does not deal with GUI stuff.
+    """
 
-	# Provide an informative description for your plug-in.
-	description = u'An example new-style plug-in'
+    # Provide an informative description for your plug-in.
+    description = u'Experiment Manager Plugin'
 
-	def reset(self):
+    def reset(self):
 
-		"""
-		desc:
-			Resets plug-in to initial values.
-		"""
+        """Resets plug-in to initial values."""
 
-		# Here we provide default values for the variables that are specified
-		# in info.json. If you do not provide default values, the plug-in will
-		# work, but the variables will be undefined when they are not explicitly
-		# set in the GUI.
-		self._value = 0
-		self._duration = 500
-		self._port = u'0x378'
+        # Set default experimental variables and values
+        self.var.pp_value = 0
+        self.var.pp_duration = 500
+        self.var.pp_dummy = u'no'
+        if os.name == 'nt':
+            self.var.pp_port = u'0x378'
+        else:
+            self.var.pp_port = u'/dev/parport0'
 
-		# add cleanup code
-		#self.experiment.cleanup_functions.append(self.clean_up_the_mess)
-  
-  	def clean_up_the_mess(self):
-		
-		if os.name == 'nt':
-			if "_winpp" in globals():
-				global _winpp
-				if self.experiment.debug:
-					print "parallel_port_trigger.clean_up_this_mess(): deleting _winpp"
-				if not _winpp is None:
-					del _winpp
-		else:
-			if "_pp" in globals():
-				global _pp
-				if self.experiment.debug:
-					print "parallel_port_trigger.clean_up_this_mess(): deleting _pp"
-				if not _pp is None:
-					del _pp
-
-	def prepare(self):
-
-		"""The preparation phase of the plug-in goes here."""
-
-		# Call the parent constructor.
-		item.prepare(self)
+        # Debugging output is only visible when OpenSesame is started with the
+        # --debug argument.
+        debug.msg(u'Parallel Port Trigger plug-in has been initialized!')
 
 
-		# get the global pp instance and initialize it if
-		# necessary#-*- coding:utf-8 -*-
+    def prepare(self):
 
-		
-		if os.name == 'nt':
-			global _winpp
-			if _winpp is None:
-				try:
-					_winpp = windll.dlportio
-					print('Successfully accessed the parallel port on address: %s' % self.get("_port"))
-				except OSError:
-					print('Could not access the parallel port on address: %s' % self.get("_port"))
-			self.winpp = _winpp
-		else:
-			global _pp
-			if _pp is None:
-				try:
-					_pp = parallel.Parallel()
-					print('Successfully accessed the parallel port (/dev/parport0).')
-				except OSError:
-					print('Could not access /dev/parport0.')
-			self.pp = _pp
+        """Preparation phase"""
 
-		
-		# create keyboard object
-		self.kb = keyboard(self.experiment, keylist=['escape'])
-		
-		# Report success
-		return True
+        # Call the parent constructor.
+        item.prepare(self)
 
-	def run(self):
+        if self.var.pp_dummy == u'no':
+            try:
+                if os.name == 'nt':
+                    self.pp = windll.dlportio
+                else:
+                    self.pp = parallel.Parallel()
+            except OSError:
+                print(u'Could not access the parallel port on address: %s' % self.var.pp_port)
+        elif self.var.pp_dummy == u'yes':
+            print(u'Dummy mode enabled, prepare phase')
+        else:
+            print(u'Error with dummy mode, mode is: %s' % self.var.pp_dummy)
 
-		"""The run phase of the plug-in goes here."""
-  		# Set the pp value
+    def run(self):
 
-		if os.name == 'nt':
-			if not self.winpp is None:
-				self.set_item_onset(self.winpp.DlPortWritePortUchar(int(self.get("_port"),0), self.get("_value")))
-				print('Sending value %s for %s ms to the parallel port on address: %s' % (self.get("_value"),self.get("_duration"),self.get("_port")))
+        """Run phase"""
 
-		else:
-			if not self.pp is None:
-				self.set_item_onset(self.pp.setData(self.get("_value")))
-				print('Sending value %s for %s ms to the parallel port on address: %s' % (self.get("_value"),self.get("_duration"),self.get("_port")))
+        # self.set_item_onset() sets the time_[item name] variable. Optionally,
+        # you can pass a timestamp, such as returned by canvas.show().
 
+        # Set the pp value
 
-		# use keyboard as timeout, allowing for Escape presses to abort experiment
-		self.kb.get_key(timeout=self.get("_duration"))
+        if self.var.pp_dummy == u'no' and self.var.pp_duration != 0:
+            ## turn trigger on
+            if os.name == 'nt':
+                self.set_item_onset(self.pp.DlPortWritePortUchar(int(self.var.pp_port,0), self.var.pp_value))
+            else:
+                self.set_item_onset(self.pp.setData(self.var.pp_value))
+            print(u'Sending value %s for %s ms to the parallel port on address: %s' % (self.var.pp_value,self.var.pp_duration,self.var.pp_port))
 
-		# unless duration was zero, turn it off
-		if os.name == 'nt':
-			if not self.winpp is None and self.get("_duration") !=0:
-				self.winpp.DlPortWritePortUchar(int(self.get("_port"),0), 0)
-				
-		else:
-			if not self.pp is None and self.get("_duration") !=0:
-				self.pp.setData(0)
-				
-		# Report success
-		return True
+            # use keyboard as timeout, allowing for Escape presses to abort experiment
+            self.kb.get_key(timeout=self.var.pp_duration)
+
+            # turn trigger off
+            if os.name == 'nt':
+                self.pp.DlPortWritePortUchar(int(self.var.pp_port,0), 0)
+            else:
+                self.pp.setData(0)
+        elif self.var.pp_dummy == u'no' and self.var.pp_duration == 0:
+            print(u'Duration is set to 0, so not doing anything')
+        elif self.var.pp_dummy == u'yes' and self.var.pp_duration != 0:
+            print(u'Dummy mode enabled, NOT sending value %s for %s ms to the parallel port on address: %s' % (self.var.pp_value,self.var.pp_duration,self.var.pp_port))
+        elif self.var.pp_dummy == u'yes' and self.var.pp_duration == 0:
+            print(u'Dummy mode enabled, duration is set to 0, so not doing anything')
+        else:
+           print(u'Error with dummy mode or duration')
+        # Report success
+        return True
+
 
 class qtparallel_port_trigger(parallel_port_trigger, qtautoplugin):
-	
-	"""
-	This class handles the GUI aspect of the plug-in. By using qtautoplugin, we
-	usually need to do hardly anything, because the GUI is defined in info.json.
-	"""
 
-	def __init__(self, name, experiment, script=None):
+    def __init__(self, name, experiment, script=None):
 
-		"""
-		Constructor.
-		
-		Arguments:
-		name		--	The name of the plug-in.
-		experiment	--	The experiment object.
-		
-		Keyword arguments:
-		script		--	A definition script. (default=None)
-		"""
+        """Experiment Manager plug-in GUI"""
 
-		# We don't need to do anything here, except call the parent
-		# constructors.
-		parallel_port_trigger.__init__(self, name, experiment, script)
-		qtautoplugin.__init__(self, __file__)
-
-	def init_edit_widget(self):
-
-		"""
-		Constructs the GUI controls. Usually, you can omit this function
-		altogether, but if you want to implement more advanced functionality,
-		such as controls that are grayed out under certain conditions, you need
-		to implement this here.
-		"""
-
-		# First, call the parent constructor, which constructs the GUI controls
-		# based on info.json.
-		qtautoplugin.init_edit_widget(self)
-		# If you specify a 'name' for a control in info.json, this control will
-		# be available self.[name]. The type of the object depends on the
-		# control. A checkbox will be a QCheckBox, a line_edit will be a
-		# QLineEdit. Here we connect the stateChanged signal of the QCheckBox,
-		# to the setEnabled() slot of the QLineEdit. This has the effect of
-		# disabling the QLineEdit when the QCheckBox is uncheckhed.
-#		self.checkbox_widget.stateChanged.connect( \
-#			self.line_edit_widget.setEnabled)
+        parallel_port_trigger.__init__(self, name, experiment, script)
+        qtautoplugin.__init__(self, __file__)
