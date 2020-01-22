16de88fdfc0c39214618e62c7bd712d44c7b5141
dev-jam <mail@bobrosbag.nl>
2020-01-23 00:16:55 +0100

    lazy init compatible

--- opensesame_plugins/parallel_port_trigger_init/parallel_port_trigger_init.py
+++ opensesame_plugins/parallel_port_trigger_init/parallel_port_trigger_init.py
@@ -28,7 +28,7 @@ from libopensesame.item import item
 from libqtopensesame.items.qtautoplugin import qtautoplugin
 from libopensesame.exceptions import osexception
 
-VERSION = u'2018.6-1'
+VERSION = u'2020.1-1'
 
 class parallel_port_trigger_init(item):
 
@@ -205,5 +205,4 @@ class qtparallel_port_trigger_init(parallel_port_trigger_init, qtautoplugin):
 
         parallel_port_trigger_init.__init__(self, name, experiment, script)
         qtautoplugin.__init__(self, __file__)
-        self.text_version.setText(
-        u'<small>Parallel Port Trigger version %s</small>' % VERSION)
+
--- opensesame_plugins/parallel_port_trigger_send/parallel_port_trigger_send.py
+++ opensesame_plugins/parallel_port_trigger_send/parallel_port_trigger_send.py
@@ -28,7 +28,7 @@ from libqtopensesame.items.qtautoplugin import qtautoplugin
 from libopensesame.exceptions import osexception
 from openexp.keyboard import keyboard
 
-VERSION = u'2018.6-1'
+VERSION = u'2020.1-1'
 
 class parallel_port_trigger_send(item):
 
@@ -162,8 +162,6 @@ class qtparallel_port_trigger_send(parallel_port_trigger_send, qtautoplugin):
 
         parallel_port_trigger_send.__init__(self, name, experiment, script)
         qtautoplugin.__init__(self, __file__)
-        self.text_version.setText(
-        u'<small>Parallel Port Trigger version %s</small>' % VERSION)
 
     def apply_edit_changes(self):
 
